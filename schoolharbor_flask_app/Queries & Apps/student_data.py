5eae7b5497a176c7244916d1f9dfae635d8725fd
nscraft <104389847+nscraft@users.noreply.github.com>
2023-12-28 10:40:33 -0700

    Update student_data.py

--- schoolharbor_flask_app/Queries & Apps/student_data.py
+++ schoolharbor_flask_app/Queries & Apps/student_data.py
@@ -1,4 +1,4 @@
-# Quire student data from schoolharbor graphql via flask app
+# Query student data from schoolharbor graphql via flask app
 from schoolharbor_flask_graphQL import fetch_data_from_graphql
 
 def get_student_data():
